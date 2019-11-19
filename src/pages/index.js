9aa315a4fb7d06b1e5b97cecd190279591662347
Ali Mahmood <ali.mahmood@arbisoft.com>
2019-11-19 18:16:18 +0500

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,51 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+import Img from "gatsby-image"
+
+const IndexPage = ({data}) => {
+  console.log(data);
+ return (
   <Layout>
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <ul>
+{data.allStrapiArticle.edges.map(article => (
+    <li key={article.node.id}>
+     <h1><Link to={`${article.node.id}`}>{article.node.title}</Link></h1>
+     <p>{article.node.content}</p>
+    <Img fixed={article.node.image.childImageSharp.fixed}/>
+    </li> 
+    )) }
+    </ul>
   </Layout>
 )
+}
 
 export default IndexPage
+
+export const pageQuery = graphql`
+ query IndexQuery {
+  allStrapiArticle {
+   edges {
+     node {
+       id
+       title
+       content
+         image {
+            childImageSharp {
+              fixed(width: 200, height: 125) {
+                ...GatsbyImageSharpFixed
+              }
+            }
+          } 
+     }
+   }
+  }
+ }
+`
